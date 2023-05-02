5b75fbd49a9796b2d5e0fd2c9eb815011ac3dc7b
mawaiszulfiqar <120576007+mawaiszulfiqar@users.noreply.github.com>
2023-05-03 04:43:38 +0500

    Add files via upload

---
+++ packages/qr/qr/qr.js
@@ -0,0 +1,11 @@
+const qrcode = require('qrcode')
+const Mailgun = require('mailgun.js')
+
+exports.main = (args) => {
+  return qrcode.toDataURL(args.text).then(res => ({
+    headers:  { 'content-type': 'text/html; charset=UTF-8' },
+    body: args.img == undefined ? res : `<img src="${res}">`
+  }))
+}
+
+if (process.env.TEST) exports.main({text:"hello"}).then(console.log)
